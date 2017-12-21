4dee6e2d8f08747b65aeeee055d49aefaa610585
otracyleeo <33510411+otracyleeo@users.noreply.github.com>
2017-12-21 21:05:39 +0800

    Create items.py

---
+++ items.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class Job51Item(scrapy.Item):
+    zhiwei = scrapy.Field()
+    gongsi = scrapy.Field()
+    didian = scrapy.Field()
+    xinzi = scrapy.Field()
+    gongsileixing = scrapy.Field()
+    guimo = scrapy.Field()
+    hangye = scrapy.Field()
+    jingyan = scrapy.Field()
+    xueli = scrapy.Field()
+    fuli = scrapy.Field()
+    zhiweiyaoqiu = scrapy.Field()
+    lianjie = scrapy.Field()
