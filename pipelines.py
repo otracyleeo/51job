a0a252d893835d7352b406ebfe5f1f7c327c0719
otracyleeo <33510411+otracyleeo@users.noreply.github.com>
2017-12-21 21:07:09 +0800

    Create pipelines.py

---
+++ pipelines.py
@@ -0,0 +1,19 @@
+from .sql import Sql
+from items import Job51Item
+
+class Job51Pipeline(object):
+    def process_item(self, item, spider):
+            zhiwei = item['zhiwei']
+            gongsi = item['gongsi']
+            didian = item['didian']
+            xinzi = item['xinzi']
+            gongsileixing = item['gongsileixing']
+            guimo = item['guimo']
+            hangye = item['hangye']
+            jingyan = item['jingyan']
+            xueli = item['xueli']
+            fuli = item['fuli']
+            zhiweiyaoqiu = item['zhiweiyaoqiu']
+            lianjie = item['lianjie']
+            Sql.insert_job51(zhiwei, gongsi, didian, xinzi, gongsileixing, guimo, hangye, jingyan, xueli, fuli, zhiweiyaoqiu, lianjie)
+            print('开始读取职位信息')
